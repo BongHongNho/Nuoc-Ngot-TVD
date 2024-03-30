5bc816cf90594f4a3d3c00b200410154da32a8fb
duy rick <duyltph38444@fpt.edu.vn>
2024-03-30 14:26:19 +0700

    them

--- app/src/main/java/com/nuocngot/tvdpro/fragment/DaGiaoFragment.java
+++ app/src/main/java/com/nuocngot/tvdpro/fragment/DaGiaoFragment.java
@@ -15,5 +15,6 @@ public class DaGiaoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_da_giao, container, false);
+
     }
 }
